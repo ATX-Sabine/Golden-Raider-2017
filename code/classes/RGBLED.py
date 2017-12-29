4d17782d126cad5d4afc3d4c54341fabee4bce22
Kas Perch <the@nodebotani.st>
2017-12-28 20:10:39 -0600

    Added common anode settings

--- code/classes/RGBLED.py
+++ code/classes/RGBLED.py
@@ -1,5 +1,5 @@
 import sys
-import syscolor
+import colorsys
 import time
 
 class RGBLED:
@@ -9,20 +9,32 @@ class RGBLED:
     self.green = pins[1]
     self.blue = pins[2]
     self.commonAnode = commonAnode
+    self.expander.startInternalClock()
+    self.expander.setDisableInputBuffer(self.red, True)
+    self.expander.setDisableInputBuffer(self.green, True)
+    self.expander.setDisableInputBuffer(self.blue, True)
+    self.expander.setPullupResistor(self.red, False)
+    self.expander.setPullupResistor(self.green, False)
+    self.expander.setPullupResistor(self.blue, False)
+    self.expander.setPinDirection(self.red, 'output')
+    self.expander.setPinDirection(self.green, 'output')
+    self.expander.setPinDirection(self.blue, 'output')
+    self.expander.setDigitalPinValue(self.red, 0)
+    self.expander.setDigitalPinValue(self.green, 0)
+    self.expander.setDigitalPinValue(self.blue, 0)
+    self.expander.startInternalClock()
+    self.expander.enableLEDDriver(self.red, True)
+    self.expander.enableLEDDriver(self.green, True)
+    self.expander.enableLEDDriver(self.blue, True)
   def on(self):
-    value = self.commonAnode ? 0x00 : 0xFF
-    self.setColor((value, value, value))
+    self.setColor([0xFF, 0xFF, 0xFF])
   def off(self):
-    value = self.commonAnode ? 0xFF : 0x00
-    self.setColor((value, value, value))
-  def setColor(color):
+    self.setColor([0x00, 0x00, 0x00])
+  def setColor(self, color):
     if self.commonAnode:
-      color = flipValues(color)
+      color = self.flipValues(color)
     self.expander.setPWMPinValue(self.red, color[0])
     self.expander.setPWMPinValue(self.green, color[1])
     self.expander.setPWMPinValue(self.blue, color[2])
-  def flipValues(color):
-    color[0] = 0xFF - color[0]
-    color[1] = 0xFF - color[1]
-    color[2] = 0xFF - color[2]
-    return color
+  def flipValues(self, color):
+    return [0xFF - color[0], 0xFF - color[1], 0xFF - color[2]]
