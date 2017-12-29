1ff24a9fa00def6bf1ff0f03b2e40dba3754dce3
Kas Perch <the@nodebotani.st>
2017-12-28 20:12:35 -0600

    Added common anode RGB LED demo

---
+++ code/examples/RGD-LED-common-anode.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append('../classes')
+sys.path.append('../SX1509-WiringPi-Python')
+from time import sleep
+
+from RGBLED import RGBLED
+from SX1509 import SX1509
+
+expander = SX1509(0x3E)
+commonAnodeRGBLED = RGBLED(expander, (5, 6, 7), True)
+
+while True:
+  print('on')
+  commonAnodeRGBLED.on()
+  sleep(1)
+  print('off')
+  commonAnodeRGBLED.off()
+  sleep(1)
