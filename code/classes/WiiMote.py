d0a53925b9c664dd42e6b5329f3f4c7942f03f78
Kas Perch <the@nodebotani.st>
2017-12-30 11:31:32 -0600

    Started scaffolding out Wiimote class

---
+++ code/classes/WiiMote.py
@@ -0,0 +1,10 @@
+import cwiid
+import time
+
+class WiiMote:
+  def __init__(self, wiimoteID):
+    # Add code to make sure it's the right wiimote horo
+  def checkForNunchuck(self):
+    # returs True if attached, False otherwise
+  def status(self):
+    # pretty prints the status of the wiimote/nunchux
