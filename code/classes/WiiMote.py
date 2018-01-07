579fd96ecd9a3e7f7e2c3e695833bf2fb91b4a4a
Kas Perch <the@nodebotani.st>
2018-01-06 23:40:16 -0600

    Scaffolding the Wiimote class

--- code/classes/WiiMote.py
+++ code/classes/WiiMote.py
@@ -1,11 +1,52 @@
 import time
 import cwiid
-from pydispatch import dispatcher
+from timeinterval import timeinterval
 
 class WiiMote:
   def __init__(self, wiimoteID):
     # Add code to make sure it's the right wiimote horo
+    print('Press 1 + 2 on the WiiMote Now')
+    try:
+      self.device=cwiid.Wiimote()
+    except RuntimeError:
+      print "Error opening wiimote connection"
+      quit()
+    
+    print 'Wii Remote connected...\n'
+
+    print 'Wii Remote connected...\n'
+    print 'Press some buttons!\n'
+    print 'Press PLUS and MINUS together to disconnect and quit.\n'
+ 
+    self.device.rpt_mode = cwiid.RPT_BTN
+
+    self.buttonHandlers = {
+      'all': [],
+      'up': [],
+      'left': [],
+      'right': [],
+      'down': [],
+      'A': [],
+      'B': [],
+      'plus': [],
+      'minus': [],
+      'home': [],
+      'one': [],
+      'two': []
+    }
+
+  # set interval to check button here
+
+
   def checkForNunchuck(self):
     # returs True if attached, False otherwise
-  def status(self):
+    return False
+
+  def on(self, handler):
+    return False
+
+  def buttonsPressed(self):
+    return []
+
+  # def status(self):
     # pretty prints the status of the wiimote/nunchux
